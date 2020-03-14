9249d7bac84d9d280379e246e8663cd346e8bf79
test.test <test@gg.com>
2020-03-13 20:33:57 -0700

    first commit

---
+++ kubernets_test-dag.py
@@ -0,0 +1,48 @@
+from airflow import DAG
+from datetime import datetime, timedelta
+from airflow.contrib.operators.kubernetes_pod_operator import KubernetesPodOperator
+from airflow.operators.dummy_operator import DummyOperator
+
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'start_date': datetime.utcnow(),
+    'email': ['airflow@example.com'],
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5)
+}
+
+
+
+dag = DAG('kubernetes_sample', description='Simple kubernetes sample DAG',
+          schedule_interval='0 12 * * *',
+          start_date=datetime(2017, 3, 20), catchup=False)
+
+start = DummyOperator(task_id='dummy_task', retries=3, dag=dag)
+
+passing = KubernetesPodOperator(namespace='s3data',
+                          image="python:3.6",
+                          cmds=["python","-c"],
+                          arguments=["print('hello world')"],
+                          labels={"foo": "bar"},
+                          name="passing-test",
+                          task_id="passing-task",
+                          get_logs=True,
+                          dag=dag,
+                          in_cluster=True
+                          )
+
+failing = KubernetesPodOperator(namespace='s3data',
+                          image="ubuntu:16.04",
+                          cmds=["python","-c"],
+                          arguments=["print('hello world')"],
+                          labels={"foo": "bar"},
+                          name="fail",
+                          task_id="failing-task",
+                          get_logs=True,
+                          dag=dag,
+                          in_cluster=True
+                          )
+start >> passing >> failing
